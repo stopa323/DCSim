4707a49cdb2ed806607af0953465bb1ea5d18a24
Bartek Stopa <stopa323@gmail.com>
2019-11-04 22:22:35 +0100

    Make object follow mouse cursor

---
+++ DataCenterSim/Assets/Scripts/BaseManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Game.Managers
+{
+    public abstract class BaseManager : MonoBehaviour
+    {
+        protected abstract void initInstance();
+
+        protected virtual void Awake()
+        {
+            initInstance();
+        }
+    }
+
+}
---
+++ DataCenterSim/Assets/Scripts/ConstructionManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace Game.Managers
+{
+    public class ConstructionManager : BaseManager
+    {
+        #region Editor Variables
+        [SerializeField] private GameObject prefab;
+        #endregion
+
+        #region Public Members
+        public static ConstructionManager Instance { get; private set; }
+        #endregion
+
+        #region Private Members
+        private GameObject obj;
+        private Camera camera;
+
+        /* Layer mask under 8th index */
+        private const int floorLayerMask = 1 << 8;
+        #endregion
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            camera = Camera.main;
+            obj = Instantiate(prefab);
+        }
+
+        protected override void initInstance()
+        {
+            if (null == Instance) { Instance = this; }
+            else if (this != Instance) { Destroy(gameObject); }
+        }
+
+        private void Update()
+        {
+            repositionBluerprintObject();
+        }
+
+        private void repositionBluerprintObject()
+        {
+            /* Moves blueprint object to position where mouse cursor is 
+             * touching floor surface. 
+             */
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100f, floorLayerMask))
+            {
+                obj.transform.position = hit.point + Vector3.up;
+            }
+        }
+    }
+}
