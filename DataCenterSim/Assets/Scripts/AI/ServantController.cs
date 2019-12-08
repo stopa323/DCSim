cbb7c500af3139262685d0dcdd18c611f5234820
Bartek Stopa <stopa323@gmail.com>
2019-12-08 16:49:31 +0100

    Add servant skeleton
    #19

---
+++ DataCenterSim/Assets/Scripts/AI/ServantController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class ServantController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+    }
+}
